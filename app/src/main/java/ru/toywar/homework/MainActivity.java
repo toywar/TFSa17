c94c3199af0c9e29b409489f92a9578d1208f6e3
Roman Ekimov <romek@toywar.ru>
2017-10-11 17:09:51 +0300

    case 1: add sharing text (implicit intent)

--- app/src/main/java/ru/toywar/homework/MainActivity.java
+++ app/src/main/java/ru/toywar/homework/MainActivity.java
@@ -1,7 +1,10 @@
 package ru.toywar.homework;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button1 = findViewById(R.id.implButton);
+        button1.setOnClickListener(new View.OnClickListener() {
+            public void onClick (View v) {
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT, "Hello, TFSa17!");
+                sendIntent.setType("text/plain");
+
+                startActivity(sendIntent);
+            }
+        });
     }
 }
